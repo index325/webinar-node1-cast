35f0c197401665b387ecf73e62af9de691b0dda5
index325 <gabriel.moraes.o@hotmail.com>
2020-09-04 02:00:06 -0300

    hello world

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => {
+  console.log("🚀Server started on port 3333");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
